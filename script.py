0a1c6472daa65ca392340a295c4d2b18d9894fea
VenkataNarasimha19 <113127012+VenkataNarasimha19@users.noreply.github.com>
2025-01-24 10:45:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,11 +4,12 @@ a=20
 b=30
 c=a+b
 d=b//a
-a
-b
-c
-d
-print(f"sum(a,b,c,d)")
+print(a)
+print(b)
+print(c)
+print(d)
+my_sum=sum(a,b,c,d)
+print(my_sum)
 mylist=[1,2,3,4,5,6,7,8,9]
 print(mylist)
 
