8005a3586c0ea1c78e8ea5d5e6733f8ca50f2315
VenkataNarasimha19 <113127012+VenkataNarasimha19@users.noreply.github.com>
2025-02-06 11:19:27 +0530

    Delete Hello.java

--- Hello.java
+++
@@ -1,7 +0,0 @@
-// Hello.java
-public class Hello {
-    public static void main(String[] args) {
-        // Print a greeting message to the console
-        System.out.println("Welcome to Jenkins and GitHub integration!. This is for testing. Added text");
-    }
-}
