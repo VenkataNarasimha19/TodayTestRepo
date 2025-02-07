acc81822e50dda67a95f808140f05067e10aebe5
VenkataNarasimha19 <113127012+VenkataNarasimha19@users.noreply.github.com>
2025-02-07 07:00:14 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
-// Hello.java
 public class Hello {
     public static void main(String[] args) {
-        // Print a greeting message to the console
-        System.out.println("Welcome to Jenkins and GitHub integration!. This is for testing. Added some text in this program");
+        System.out.println("Hello, TeamCity!");
     }
 }
